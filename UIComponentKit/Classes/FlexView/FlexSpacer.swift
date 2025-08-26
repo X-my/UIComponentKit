d5feefed915fa7cb89700ddc9a56c51daac488b3
xumengyang <woshixmy@163.com>
2025-08-26 10:37:24 +0800

    Update FlexSpacer.swift

--- UIComponentKit/Classes/FlexView/FlexSpacer.swift
+++ UIComponentKit/Classes/FlexView/FlexSpacer.swift
@@ -7,11 +7,38 @@
 
 import UIKit
 
-extension FlexNode {
-    public mutating func spacer(width: CGFloat = 0, height: CGFloat = 0) -> FlexNode {
+public enum FlexSpacerMode {
+    case horizontal(CGFloat)
+    case vertical(CGFloat)
+    case expand
+}
+
+public extension FlexNode {
+    mutating func spacer(_ mode: FlexSpacerMode) {
         node(from: FlexView.self) {
-            $0.flexConfig.width = width
-            $0.flexConfig.height = height
+            switch mode {
+            case .horizontal(let size):
+                $0.flexConfig.width = size
+            case .vertical(let size):
+                $0.flexConfig.height = size
+            case .expand:
+                $0.flexConfig.flexGrow = 1
+            }
+        }
+    }
+}
+
+public extension FlexNode.FlexWrapper {
+    mutating func spacer(_ mode: FlexSpacerMode) {
+        node(from: FlexView.self) {
+            switch mode {
+            case .horizontal(let size):
+                $0.flexConfig.width = size
+            case .vertical(let size):
+                $0.flexConfig.height = size
+            case .expand:
+                $0.flexConfig.flexGrow = 1
+            }
         }
     }
 }
